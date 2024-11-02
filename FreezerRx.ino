c71cbec42e1cf71c09b0a7fdfb6c5b0019686fde
Geoff Day <gda26470@gmail.com>
2024-11-03 09:30:15 +1030

    Update FreezerRx.ino
    Using base 36. receives  temps and trends.

--- FreezerRx.ino
+++ FreezerRx.ino
@@ -5,8 +5,10 @@
 // initialize the library by associating any needed LCD interface pinc with the arduino pin number it is connected to
 const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 int count = 0;
 char msg[10];
+long tmp = 0;
 char *str;
 char **endptr;
 int t1 = 0;
@@ -24,21 +26,28 @@ bool ot3 = false;
 bool fault1 = false;
 bool fault2 = false;
 bool fault3 = false;
+int pause = 0;
 
 void setup() 
 {
   lcd.begin(16, 2);                 // set up the LCD's number of columns and rows:
+  lcd.display();
   lcd.print("Reciever");       // Print a message to the LCD.
   
   Serial.begin(9600);
   while (!Serial);
   Serial.println("LoRa Receiver");
+
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+
+  str = (char*)msg;
+  endptr = &str;
 }
- 
+
+
 void loop() {
   // try to parse packet
   int packetSize = LoRa.parsePacket();
@@ -48,129 +57,108 @@ void loop() {
     Serial.println("");
     Serial.println("...................................");
     Serial.println("Received packet: ");
+    Serial.println(packetSize);
+    // lcd.begin(16,2);
     lcd.setCursor(0, 1);
+    strcpy(msg,"   ");
+    str = (char*)msg;
+    endptr = &str;
+    count = 0;
+
     // read packet
     while (LoRa.available()) {
-      char incoming = (char)LoRa.read();
-       
-      lcd.print(incoming);
-      if ((incoming == 'n') || (incoming == 'p')) {
-        count = 0;
+      char incoming = (char)LoRa.read();    
+      
+      if (incoming == '|') pause = 0;     // n is normal mode - alarms on
+      if (incoming == '~') pause = 1800;  // p is paused mode - alarms off for 30 mins. freezer opened
+
+      if ((incoming == '|') || (incoming == '~')){
+       count = 0; 
       } else {
+        msg[count] = incoming;
         count += 1;
       }
+      
+      if (incoming == '.') {
+        // tmp = strtoull(str, endptr, 36);
+        Serial.println(str);
+        tmp = strtol(str, 0, 36);
+        t1 = (tmp & 0x3ff00000) >> 20;
+        t2 = (tmp & 0x000FFC00) >> 10;
+        t3 = tmp & 0x000003ff;
+        Serial.println(tmp);
+        Serial.println(count);
 
-      switch (count){
-        case 0: break;
+        if (t1 > 512) {
+          t1 -= 512;
+          trend1 = true;
+        } else {
+          trend1 = false;
+        }
 
-        case 1:
-        case 2:
-        case 3: msg[count - 1] = incoming;
-                t1 = strtoul(msg, 3, 16);
+        temp1 = map(t1, 0, 511, -150, 361)/10.0;
 
-                if (t1 > 2048) {
-                  t1 -= 2048;
-                  trend1 = true;
-                } else {
-                  trend1 = false;
-                }
+        if (t2 > 512) {
+          t2 -= 512;
+          trend2 = true;
+        } else {
+          trend2 = false;
+        }
+        
+        temp2 = map(t2, 0, 511, -150, 361)/10.0;
 
-                if (t1 > 1024) {
-                  t1 -= 1024;
-                  ot1 = true;
-                } else {
-                  ot1 = false;
-                }
+        if (t3 > 512) {
+          t3 -= 512;
+          trend3 = true;
+        } else {
+          trend3 = false;
+        }
 
-                if (t1 > 512) {
-                  t1 -= 512;
-                  fault1 = true;
-                } else {
-                  fault1 = false;
-                }
-                break;
-
-        case 4:
-        case 5:
-        case 6: msg[count - 4] = incoming;
-                t2 = strtoul(msg, 3, 16);
-
-                if (t2 > 2048) {
-                  t2 -= 2048;
-                  trend2 = true;
-                } else {
-                  trend2 = false;
-                }
-
-                if (t2 > 1024) {
-                  t2 -= 1024;
-                  ot2 = true;
-                } else {
-                  ot2 = false;
-                }
-
-                if (t2 > 512) {
-                  t2 -= 512;
-                  fault2 = true;
-                } else {
-                  fault2 = false;
-                }
-                break;
-
-        case 7:
-        case 8:
-        case 9: msg[count - 7] = incoming;
-                t3 = strtoul(msg, 3, 16);
-
-                if (t3 > 2048) {
-                  t3 -= 2048;
-                  trend3 = true;
-                } else {
-                  trend3 = false;
-                }
-
-                if (t3 > 1024) {
-                  t3 -= 1024;
-                  ot3 = true;
-                } else {
-                  ot3 = false;
-                }
-
-                if (t3 > 512) {
-                  t3 -= 512;
-                  fault3 = true;
-                } else {
-                  fault3 = false;
-                }                
-                break;
-
-        default: break;
+        temp3 = map(t3, 0, 511, -150, 361)/10.0;
       }
 
-                  Serial.println(msg);
-                  Serial.println(t1);
-                  Serial.println(trend1);
-                  Serial.println(ot1);
-                  Serial.println(fault1);
-                  Serial.println(t2);
-                  Serial.println(trend2);
-                  Serial.println(ot2);
-                  Serial.println(fault2);
-                  Serial.println(t3);
-                  Serial.println(trend3);
-                  Serial.println(ot3);
-                  Serial.println(fault3);
+      lcd.print(incoming);
+
+      // Serial.println(msg);
+
+
+      lcd.setCursor(0,0);
+      lcd.clear();
+      lcd.print(temp1, 1);
+      lcd.print(" ");
+      lcd.print(temp2, 1);
+      lcd.print(" ");
+      lcd.print(temp3, 1); 
+      lcd.println("  ");  
+      
+      lcd.setCursor(0,1);
+
+      if (pause == 0) {
+        if ((temp1 <-20.0) || (temp2 <-20.0) || (temp3 <-20.0)) lcd.println("Sens. Fault ");
+        if ((temp1 > 0.0) || (temp2 > 0.0) || (temp3 > 0.0)){
+          lcd.print("Over Temp  ");
+        } else {
+          lcd.print("below zero ");
+        }
+
+        lcd.println(LoRa.rssi());
+      } else {
+        pause -= 600;                 // this loop goes every 10mins or 600 seconds
+        lcd.print("Al. Paused ");
+        lcd.print(pause/60);
+        lcd.println(" min");
+      }
+    
+
+
+      Serial.println(msg);
+      Serial.println(temp1, 1);
+      Serial.println(temp2, 1);
+      Serial.println(temp3, 1);
+
+      Serial.println(LoRa.rssi());
+
+
     }
   }
-
-
 }
-// int reconstructReading(float temp, float prev){
-//   short tVal = short(temp * 10);
-  
-//   tVal = map(tVal, -150, 361, 0, 511);
-//   if (temp <-20.0) bitSet(tVal, 9);     // Fault - if temp below -20C then it's most likely a faulty sensor
-//   if (temp > 0.0) bitSet(tVal, 10);     // Over temp - if its above zero we have a problem so alarm again
-//   if (temp <= prev) bitSet(tVal, 11);   // Trend - if temp less than or equal to previous then its OK
-//   return tVal;
-// }
